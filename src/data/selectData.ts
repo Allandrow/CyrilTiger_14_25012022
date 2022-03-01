8cc1475611e57aeb06b255c07e7b79787491cbbb
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-03-01 15:42:47 +0100

    added datatable package

--- src/data/selectData.ts
+++ src/data/selectData.ts
@@ -262,3 +262,15 @@ const employees: Employee[] = [
 ]
 
 export { states, departments, employees }
+
+export const columns = [
+  { key: 'firstName', header: 'First Name' },
+  { key: 'lastName', header: 'Last Name' },
+  { key: 'startDate', header: 'Start Date', sortMethod: 'sortDateISO' },
+  { key: 'department', header: 'Department' },
+  { key: 'dateOfBirth', header: 'Date of Birth', sortMethod: 'sortDateISO' },
+  { key: 'street', header: 'Street' },
+  { key: 'city', header: 'City' },
+  { key: 'state', header: 'State' },
+  { key: 'zipCode', header: 'Zip Code', sortMethod: 'sortNumber' },
+]
--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,6 +1,10 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
+import { DataTable } from 'react-ts-datatable'
 import { Header } from '../components/header/Header'
+import { columns } from '../data/selectData'
+import '../styles/table.css'
 
 const FIVE_MINUTES = 1000 * 60 * 5
 
@@ -10,14 +14,22 @@ const getEmployees = async () => {
 }
 
 export const Employees = () => {
-  const employeeList = useQuery('employees', getEmployees, {
+  const [employeeList, setEmployeeList] = useState([])
+  const employeeListQuery = useQuery('employees', getEmployees, {
     refetchOnWindowFocus: false,
     staleTime: FIVE_MINUTES,
   })
-  console.log(employeeList)
+
+  useEffect(() => {
+    if (employeeListQuery.status === 'success') {
+      setEmployeeList(employeeListQuery.data)
+    }
+  }, [employeeListQuery.status])
+
   return (
     <>
       <Header link="/" text="Home" />
+      <DataTable data={employeeList} columns={columns} />
     </>
   )
 }
